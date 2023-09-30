0a4cb5f89e69f7729c51906a6324398fd3047a27
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2023-09-30 15:50:04 -0400

    added view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
+// src/App.js
+
 import React from 'react';
 import { Grommet } from 'grommet';
-import logo from './logo.svg';
+import PostsList from './components/PostsList';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <Grommet full>
+    <Grommet>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Welcome to Altama BBS</h1>
         </header>
+        <main>
+          <PostsList />
+          {/* Other components go here */}
+        </main>
       </div>
     </Grommet>
   );
