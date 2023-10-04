a79f2cd97218430005435eec6363353a3a6d49ef
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2023-10-04 16:21:44 -0400

    debugging

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Redirect } from 'react-router-dom';
 import PostsList from './components/PostsList';
 import AppLayout from './components/AppLayout';
 import './App.css';
@@ -13,13 +13,12 @@ function App() {
         <Routes>
           <Route path="/post/:postId">
             {/* Placeholder for individual post details. You can create and import a PostDetail component later. */}
-            {/* <PostDetail /> */}
             <div>Post Detail Placeholder</div>
           </Route>
           
-          <Route path="/" exact>
-            <PostsList />
-          </Route>
+          <Route path="/forums" element={<PostsList />} />
+
+          <Route path="/" element={<Redirect to="/forums" />} />
         </Routes>
       </AppLayout>
     </Router>
