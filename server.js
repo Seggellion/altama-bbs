40a47547db86bb25cfaedc4858b22aac57237185
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2023-10-04 14:47:50 -0400

    added debugging

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,27 +11,37 @@ const pool = new Pool({
     ssl: {
       rejectUnauthorized: false
     }
-  });
+});
+
+// Middleware
+app.use(cors()); // Allow all origins for simplicity. Refine for production.
+app.use(express.json());
+
+// API endpoint to fetch all posts
+app.get('/api/posts', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM forum_posts');  // Assuming your table name is "forum_posts"
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch posts' });
+    }
+});
+
+// Test API endpoint
+app.get('/api/test', (req, res) => {
+    console.log("API Test route hit");
+    res.send('Hello World');
+});
 
 // Serve static files from the React app build
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Handles any requests that don't match the above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
-
-app.get('/api/posts', async (req, res) => {
-    try {
-      const result = await pool.query('SELECT * FROM posts');  // Assuming your table name is "posts"
-      res.json(result.rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to fetch posts' });
-    }
-  });
-  
