fbebd1640d268a5fba3914212f8f601ed1ce73ad
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2023-10-04 15:52:06 -0400

    Update react-router-dom usage for v6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PostsList from './components/PostsList';
 import AppLayout from './components/AppLayout';
 import './App.css';
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <AppLayout>
-        <Switch>
+        <Routes>
           <Route path="/post/:postId">
             {/* Placeholder for individual post details. You can create and import a PostDetail component later. */}
             {/* <PostDetail /> */}
@@ -20,7 +20,7 @@ function App() {
           <Route path="/" exact>
             <PostsList />
           </Route>
-        </Switch>
+        </Routes>
       </AppLayout>
     </Router>
   );
