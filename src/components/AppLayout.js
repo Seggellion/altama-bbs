7799c5aa0c39472edbe40d8333dd48259773057c
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2023-10-05 00:43:27 -0700

    added interface change

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Grommet, Box, Header, Main, Sidebar, Text } from 'grommet';
 import CustomSidebarContent from './Sidebar';  // Adjust the path based on your directory structure
 
-import { grommet } from 'grommet/themes';
-import customTheme from '../path-to-theme/theme';  // Adjust path
+import customTheme from './theme/theme';  // Adjust the path to where you saved theme.js
+
+
 
 
 function AppLayout({ children }) {
   return (
-    <Grommet theme={grommet} full>
+    <Grommet theme={customTheme} full>
       <Box fill>
         <Header background="brand" pad="medium">
         <Text size="large" weight="bold" color="accent-1">Altama BBS</Text>
---
+++ theme/index.js
@@ -0,0 +1,37 @@
+import { deepMerge } from 'grommet/utils';
+import { grommet } from 'grommet/themes';
+
+const customTheme = deepMerge(grommet, {
+    global: {
+      colors: {
+        brand: '#444444',
+        'accent-1': '#FFCC00',
+        'light-1': '#f0f0f0',
+        'light-2': '#d6d6d6',
+        focus: 'transparent', // Disable Grommet's default focus ring
+      },
+      font: {
+        family: `'Courier New', Courier, monospace`,
+      },
+      edgeSize: {
+        small: '14px',
+      },
+    },
+    text: {
+      extend: {
+        fontFamily: `'Lucida Console', Monaco, monospace`,
+      },
+    },
+    button: {
+      border: {
+        radius: '0',
+      },
+      padding: {
+        horizontal: '20px',
+        vertical: '10px',
+      },
+      extend: {
+        fontFamily: `'Lucida Console', Monaco, monospace`,
+      },
+    },
+  });
