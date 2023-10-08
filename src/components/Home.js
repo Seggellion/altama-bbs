8de4d4de39f802e8348b2f4db1c41787d7a6e70d
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2023-10-07 18:02:33 -0700

    Refactored ANSI graphics

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,42 @@
 import React, { useEffect, useState } from 'react';
 
-
 const Home = () => {
-    const [ansiContent, setAnsiContent] = useState('');
-  
-    useEffect(() => {
-        
-        
-        const script = document.createElement('script');
-        debugger;
-        script.src = '/js/ansilove.js'; // Adjust the path as needed
-        script.onload = () => {
-          // The script has loaded, do something
-          if (window.AnsiLove) {
-            console.log("AnsiLove loaded:", window.AnsiLove);
-          }
-        };
-        document.body.appendChild(script);
+  const [ansiContent, setAnsiContent] = useState('');
 
-      // Assume ansilove is a global object exposed by ansilove.js
-      // If it's not global, you might need to import it differently
-      const ansilove = window.ansilove;
-  
-      fetch('/assets/altama_bbs_graphic.ans')  // Updated the fetch URL
-        .then(response => response.text())
-        .then(text => {
-          const renderedAnsi = ansilove.render(text, { /* your options here */ });
-          setAnsiContent(renderedAnsi);
-        });
-    }, []);
-  
-    return (
-      <div>
-        {/* Display the rendered ANSI. This will depend on how ansilove.js renders the ANSI. */}
-        <div dangerouslySetInnerHTML={{ __html: ansiContent }}></div>
-      </div>
-    );
-  };
-  
+  useEffect(() => {
+    // Dynamically load ansilove.js
+    const script = document.createElement('script');
+    script.src = '/js/ansilove.js'; // Adjust the path as needed
+    script.onload = () => {
+      // The script has loaded, now fetch the ANSI file
+      if (window.AnsiLove) {
+        console.log("AnsiLove loaded:", window.AnsiLove);
+
+        fetch('/assets/altama_bbs_graphic.ans')
+          .then((response) => response.arrayBuffer())
+          .then((buffer) => {
+            const options = { retina: false };
+            const canvas = window.AnsiLove.render(buffer, options);
+            setAnsiContent(canvas);
+          })
+          .catch((error) => {
+            console.log("Error fetching ANSI file:", error);
+          });
+      }
+    };
+    document.body.appendChild(script);
+
+    return () => {
+      // Cleanup: Remove the script when the component unmounts
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div>
+      {ansiContent}
+    </div>
+  );
+};
 
 export default Home;
