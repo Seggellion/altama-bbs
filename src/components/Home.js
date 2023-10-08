10223984a79825d876cf707b175c4d3824903668
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2023-10-07 17:06:05 -0700

    debugging

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,35 @@ import React, { useEffect, useState } from 'react';
 import Ansi from 'ansi-to-react';
 
 const Home = () => {
-  const [ansiContent, setAnsiContent] = useState('');
-
-  useEffect(() => {
-    // Fetch ANSI file and update state
-    fetch('/altama_bbs_graphic.ans')
-      .then((response) => response.text())
-      .then((text) => setAnsiContent(text));
-  }, []);
-
-  return (
-    <div>
-      <Ansi>{ansiContent}</Ansi>
-    </div>
-  );
-};
+    const [ansiContent, setAnsiContent] = useState('');
+  
+    useEffect(() => {
+      console.log("Fetching ANSI file...");
+      
+      fetch('/altama_bbs_graphic.ans')
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.text();
+        })
+        .then((text) => {
+          console.log("Successfully fetched ANSI file.");
+          setAnsiContent(text);
+        })
+        .catch((error) => {
+          console.log("Error fetching ANSI file:", error);
+        });
+    }, []);
+  
+    console.log("Rendering ANSI content:", ansiContent);
+  
+    return (
+      <div>
+        <Ansi>{ansiContent}</Ansi>
+      </div>
+    );
+  };
+  
 
 export default Home;
