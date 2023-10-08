c43fa58b7531ae2f2f4ad7d39d7d3d3b6af36dd1
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2023-10-07 17:39:56 -0700

    debugging

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import Ansi from 'ansi-to-react';
 
+
+
 const Home = () => {
-  const [ansiContent, setAnsiContent] = useState('');
-
-  useEffect(() => {
-    console.log("Fetching ANSI file...");
-
-    fetch('/assets/altama_bbs_graphic.ans')  // Updated the fetch URL
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.text();
-      })
-      .then((text) => {
-        console.log("Successfully fetched ANSI file.", text);
-        setAnsiContent(text);
-      })
-      .catch((error) => {
-        console.log("Error fetching ANSI file:", error);
-      });
-  }, []);
-
-  console.log("Rendering ANSI content:", ansiContent);
-
-  return (
-    <div>
-      <Ansi>{ansiContent}</Ansi>
-    </div>
-  );
-};
+    const [ansiContent, setAnsiContent] = useState('');
+  
+    useEffect(() => {
+      // Assume ansilove is a global object exposed by ansilove.js
+      // If it's not global, you might need to import it differently
+      const ansilove = window.ansilove;
+  
+      fetch('/assets/altama_bbs_graphic.ans')  // Updated the fetch URL
+        .then(response => response.text())
+        .then(text => {
+          const renderedAnsi = ansilove.render(text, { /* your options here */ });
+          setAnsiContent(renderedAnsi);
+        });
+    }, []);
+  
+    return (
+      <div>
+        {/* Display the rendered ANSI. This will depend on how ansilove.js renders the ANSI. */}
+        <div dangerouslySetInnerHTML={{ __html: ansiContent }}></div>
+      </div>
+    );
+  };
+  
 
 export default Home;
