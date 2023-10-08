a21bcc542d687a17e75220c8b31f644ff337db95
Old Shopify computer <oldshopifycomputer@Olds-MacBook-Pro.local>
2023-10-07 17:19:44 -0700

    debugging

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,35 +2,34 @@ import React, { useEffect, useState } from 'react';
 import Ansi from 'ansi-to-react';
 
 const Home = () => {
-    const [ansiContent, setAnsiContent] = useState('');
-  
-    useEffect(() => {
-      console.log("Fetching ANSI file...");
-      
-      fetch('/public/assets/altama_bbs_graphic.ans')
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          return response.text();
-        })
-        .then((text) => {
-          console.log("Successfully fetched ANSI file.", text);
-          setAnsiContent(text);
-        })
-        .catch((error) => {
-          console.log("Error fetching ANSI file:", error);
-        });
-    }, []);
-  
-    console.log("Rendering ANSI content:", ansiContent);
-  
-    return (
-      <div>
-        <Ansi>{ansiContent}</Ansi>
-      </div>
-    );
-  };
-  
+  const [ansiContent, setAnsiContent] = useState('');
+
+  useEffect(() => {
+    console.log("Fetching ANSI file...");
+
+    fetch('/assets/altama_bbs_graphic.ans')  // Updated the fetch URL
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.text();
+      })
+      .then((text) => {
+        console.log("Successfully fetched ANSI file.", text);
+        setAnsiContent(text);
+      })
+      .catch((error) => {
+        console.log("Error fetching ANSI file:", error);
+      });
+  }, []);
+
+  console.log("Rendering ANSI content:", ansiContent);
+
+  return (
+    <div>
+      <Ansi>{ansiContent}</Ansi>
+    </div>
+  );
+};
 
 export default Home;
